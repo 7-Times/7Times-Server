4ab2e7cd2cfdb4c3b60a46c2c15c6a9efa62702c
DoguCigsar <dogucigsar@hotmail.com>
2022-03-21 15:51:02 +0300

    Test

---
+++ index.js
@@ -0,0 +1,15 @@
+//A server that responds with hello world
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send({ 
+    message: 'Hello World!'
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
