9fb7127400bf3ad389e0f79f1ba0ee9418a6e16a
DoguCigsar <dogucigsar@hotmail.com>
2022-03-22 17:23:19 +0300

    CORS

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors')
+app.use(cors({
+  origin: '*',
+}))
 
 app.get('/', (req, res) => {
   res.send({ 
